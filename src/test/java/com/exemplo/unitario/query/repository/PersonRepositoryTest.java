9ce644edd61c9ef95444c31352dc39765c7363b5
Lucas Miranda <lucas.guimaraes@qintess.com>
2021-12-28 14:28:05 -0300

    Implementando testes

--- src/test/java/com/exemplo/unitario/query/repository/PersonRepositoryTest.java
+++ src/test/java/com/exemplo/unitario/query/repository/PersonRepositoryTest.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
+
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 @DataJpaTest
 class PersonRepositoryTest {
+
 
     @Autowired
     private PersonRepository personRepository;
@@ -21,8 +23,14 @@ class PersonRepositoryTest {
     @Sql("/sql/insert-person.sql")
     void findBySurnameQuery() {
         assertNotNull(personRepository);
-        List<Person> persons = personRepository.findBySurnameQuery("Damasco");
-        System.err.println(persons);
+
+        personRepository.save(new Person("Lucas", "Miranda", "1519"));
+
+        List<Person> personList = personRepository.findBySurnameQuery("Miranda");
+        assertEquals(2, personList.size());
+        personList.forEach(person -> assertEquals("Miranda", person.getSurname()));
+
+        System.err.println(personList);
 
     }
 }
--- src/test/resources/sql/insert-person.sql
+++ src/test/resources/sql/insert-person.sql
@@ -1,2 +1,2 @@
 INSERT INTO person (cpf, name, surname) VALUES ('963', 'Teresa', 'Damasco');
-INSERT INTO person (cpf, name, surname) VALUES ('678', 'Ana', 'Cristina');
+INSERT INTO person (cpf, name, surname) VALUES ('678', 'Ana', 'Miranda');
